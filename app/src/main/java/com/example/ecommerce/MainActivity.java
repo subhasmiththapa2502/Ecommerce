675a93f2c117c24dc67c8c2a75919007b98c56fa
Subhasmith <subhsamith@indee.tv>
2021-10-20 16:05:57 +0530

    Bottom Nav Sorted

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -5,35 +5,71 @@ import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.ecommerce.ui.HomeFragment;
 import com.example.ecommerce.ui.ProfileFragment;
 import com.example.ecommerce.utils.Utils;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.chipNav)
+    ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         init();
 
-        // Begin the transaction
+        chipNavigationBar.setItemSelected(R.id.home,
+                true);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.your_placeholder,
+                        new HomeFragment()).commit();
+        bottomMenu();
+        /*// Begin the transaction
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 // Replace the contents of the container with the new fragment
         ft.replace(R.id.your_placeholder, new ProfileFragment());
 // or ft.add(R.id.your_placeholder, new FooFragment());
 // Complete the changes added above
-        ft.commit();
+        ft.commit();*/
     }
 
-    public void init(){
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener
+                (new ChipNavigationBar.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(int i) {
+                        Fragment fragment = null;
+                        switch (i){
+                            case R.id.home:
+                                fragment = new HomeFragment();
+                                break;
+                            case R.id.profile:
+                                fragment = new ProfileFragment();
+                                break;
+                        }
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.your_placeholder,
+                                        fragment).commit();
+                    }
+                });
+    }
+    public void init() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
         Utils.changeStatusBarColour(this);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/ecommerce/ui/HomeFragment.java
@@ -0,0 +1,10 @@
+package com.example.ecommerce.ui;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Subhasmith Thapa on 20,October,2021
+ */
+public class HomeFragment extends Fragment {
+
+}
